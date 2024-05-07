016e0c4ea5b6439b05c2951a066052ad8aa04a22
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-05-07 20:14:00 +0300

    Create MainController, install cs-fixer

---
+++ src/Controller/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+    #[Route('/', )]
+    public function homepage() : Response
+    {
+        return new Response('This is a test string');
+    }
+}
