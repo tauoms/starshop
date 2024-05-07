8e48e63fe87b7b5b1716ed470a6adb82f062de94
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-05-07 20:17:19 +0300

    Fix with cs-fixer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController
 {
     #[Route('/', )]
-    public function homepage() : Response
+    public function homepage(): Response
     {
         return new Response('This is a test string');
     }
