b490df1c37c260637720c90ac51427e4cada5233
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-05-16 22:02:16 +0300

    Install turbo, finalize project

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
